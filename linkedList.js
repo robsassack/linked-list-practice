1c981bc5f4334cd7b63b738b19a3ff25ace4753e
Rob Sassack <rsassack25@gmail.com>
2022-09-13 13:05:07 -0400

    Added appending

--- linkedList.js
+++ linkedList.js
@@ -1,7 +1,43 @@
 class LinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+  }
 
+  append(value) {
+    const newNode = new Node(value);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return;
+    }
+    this.tail.nextNode = newNode;
+    this.tail = newNode;
+    return;
+  }
+
+  toString() {
+    let current = this.head;
+    let str = '';
+    while (current) {
+      str += `( ${current.value} ) -> `;
+      current = current.nextNode;
+    }
+    return str + 'null';
+  }
 }
 
 class Node {
-
+  constructor(value) {
+    this.value = value;
+    this.nextNode = null;
+  }
 }
+
+let list = new LinkedList();
+list.append(1);
+list.append(2);
+list.append(3);
+list.append(4);
+list.append(5);
+console.log(list.toString());
